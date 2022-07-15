e382b16a921327e695ae5c4c40c9aae567878108
helio-lopes13 <heliosgod13@hotmail.com>
2022-07-15 19:12:13 -0300

    Terceira aula - comentários

---
+++ s1-js-basico/comentarios.js
@@ -0,0 +1,12 @@
+console.log('<Meu Deus/>'); // esse comentário vai na mesma linha
+
+// Aqui é um comentário de linha
+
+/**
+ * Esse é um comentário de
+ * bloco-chan
+ */
+console.log('Linha 1');
+console.log('Linha 2');
+// console.log('Linha 3'); // esse aqui não vai
+console.log('Linha 4');
