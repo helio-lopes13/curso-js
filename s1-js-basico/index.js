accdd5438c77186c3470ac9a3fda54de32fe4c34
helio-lopes13 <heliosgod13@hotmail.com>
2022-07-15 19:00:09 -0300

    Segunda aula - exercício

--- s1-js-basico/index.js
+++ s1-js-basico/index.js
@@ -1,5 +1,9 @@
+// Aula sobre console.log()
 console.log('Klee der "Mont"');
 console.log("'Klee' der Mont");
 console.log(`'Klee' der "Mont"`);
 
 console.log(123456);
+
+// Exercício
+console.log('Meu nome é "Luni". Estou aprendendo JavaScript às', 7, 'da noite.');
